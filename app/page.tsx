2d696563ce29c543887a28e4c9bcecbffb7f7cf0
Temporal 76 <marc.pollet76@gmail.com>
2023-09-21 16:45:40 +0200

    added conditional rendering on greeting H1 in <Index/>

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
 "use client"
 
 import { useContext, useEffect } from "react"
+import { Separator } from "@radix-ui/react-separator"
 import { AnimatePresence, motion } from "framer-motion"
 import { Loader } from "lucide-react"
 
 import { TodosContext } from "@/components/context/todosContext"
 import DropDownFilter from "@/components/dropDownFilter"
+import Stats from "@/components/stats"
 import TodoForm from "@/components/todo-form"
 import TodoItem from "@/components/todo-item"
-import Stats from "@/components/stats"
-import { Separator } from "@radix-ui/react-separator"
-
 
 export default function IndexPage() {
   const { todos, selectedTodos, loadDemoTodos, todoAreLoading } =
@@ -20,16 +19,31 @@ export default function IndexPage() {
     return todos && todos.length > 0
   }
 
+  const computeGreetingText = () => {
+    if (todoAreLoading) return "Bienvenue"
+    if (isTodo()) return "Bienvenue ! Jetons un œil à vos tâches en cours."
+    return "Bienvenue, vous n'avez aucune tâche en cours !"
+  }
+
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <h1 className="text-center text-sm md:text-3xl mx-auto">
-        Bienvenue ! Jetons un œil à vos tâches en cours
-      </h1>
+      <AnimatePresence>
+        <motion.div
+      
+          initial={{ opacity: 0, scale: 0.98 }} 
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0.98 }} 
+          transition={{ duration: 0.2, ease: "easeInOut" }} 
+          className="text-center text-sm md:text-3xl mx-auto "
+        >
+          {computeGreetingText()}
+        </motion.div>
+      </AnimatePresence>
       {/* <div className="w-full container mx-auto">
         <Stats />
         <Separator className="my-4" />
       </div> */}
-  
+
       <div className="flex flex-col md:flex-row items-center w-full justify-evenly md:flex-nowrap flex-wrap gap-7 mx-auto container">
         <div className="w-auto md:w-full mx-auto ">
           <TodoForm />
@@ -61,12 +75,12 @@ export default function IndexPage() {
       </motion.div>
       {todoAreLoading && <Loader className="animate-spin w-full mx-auto" />}
       {!isTodo() && !todoAreLoading ? (
-      <h1 className="text-center text-muted-foreground w-full mx-auto">
-      <TodoForm displayValue=" Créer votre première tâche" /> ou{" "}
-      <button className="underline" onClick={loadDemoTodos}>
-        importer des exemples
-      </button>
-    </h1>
+        <h1 className="text-center text-muted-foreground w-full mx-auto">
+          <TodoForm displayValue=" Créer votre première tâche" /> ou{" "}
+          <button className="underline" onClick={loadDemoTodos}>
+            importer des exemples
+          </button>
+        </h1>
       ) : null}
     </section>
   )
